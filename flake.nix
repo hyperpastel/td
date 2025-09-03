834c8c1725d651e07bfcdc75e7ecc83121bf970d
hyperpastel <vmutze04@gmail.com>
2025-09-03 02:01:12 +0200

    refactor(flake): move c3c to flake input

--- flake.nix
+++ flake.nix
@@ -4,40 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    c3c = {
+        url = "github:c3lang/c3c";
+        inputs.nixpkgs.follows = "nixpkgs";
+        inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-
-    flake-utils.lib.eachDefaultSystem (
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
       system:
-      # Required until https://github.com/NixOS/nixpkgs/pull/435544/ gets merged
       let
-        c3c-overlay = (
-          final: prev: {
-            c3c = prev.c3c.overrideAttrs (old: {
-              version = "0.7.5";
-              doCheck = false;
-              src = prev.fetchFromGitHub {
-                owner = "c3lang";
-                repo = "c3c";
-                rev = "c0387221af7e9668b5fdde335b06e5bc89bc79fb";
-                hash = "sha256-ukDjNqWqMboqOrm+dLTQZ03D7SP2DcQF22/n/CEZSMs=";
-              };
-            });
-          }
-        );
-        pkgs = (nixpkgs.legacyPackages.${system}.extend c3c-overlay);
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
       in
       with pkgs;
       {
-        devShells.default = mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            c3c
+            inputs.c3c.packages.${system}.default
             c3-lsp
           ];
 
